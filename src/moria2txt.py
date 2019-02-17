2ba77b437240109e9a76cf2ff943ab5482d2ce80
Monofoot <Robertofield@hotmail.co.uk>
2019-02-17 21:34:57 +0000

    Random normal distribution function now works, need to start working on the actual meat of the generator now. Good progress though, feeling good

--- src/moria2txt.py
+++ src/moria2txt.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import time
 
+# TO-DO: Make another Python file for your constants and import from them.
+
 # Parameters for the Lehmer generator using
 # Schrage's implementation.
 # 2^31 - 1
@@ -8,6 +10,51 @@ RNG_M = 2**31 - 1
 RNG_A = 48271
 RNG_Q = RNG_M / RNG_A
 RNG_R = RNG_M % RNG_A
+
+# All constants should be stored here #
+# Hold a parameter for MAX_SHORT int.
+MAX_SHORT = 32767
+# Definitions for the normal distribution function
+NORMAL_TABLE_SIZE = 256
+NORMAL_TABLE_SD = 64
+normal_table = [206,     613,    1022,    1430,    1838,    2245,    2652,    3058,
+    3463,    3867,    4271,    4673,    5075,    5475,    5874,    6271,
+    6667,    7061,    7454,    7845,    8234,    8621,    9006,    9389,
+    9770,   10148,   10524,   10898,   11269,   11638,   12004,   12367,
+    12727,   13085,   13440,   13792,   14140,   14486,   14828,   15168,
+    15504,   15836,   16166,   16492,   16814,   17133,   17449,   17761,
+    18069,   18374,   18675,   18972,   19266,   19556,   19842,   20124,
+    20403,   20678,   20949,   21216,   21479,   21738,   21994,   22245,
+    22493,   22737,   22977,   23213,   23446,   23674,   23899,   24120,
+    24336,   24550,   24759,   24965,   25166,   25365,   25559,   25750,
+    25937,   26120,   26300,   26476,   26649,   26818,   26983,   27146,
+    27304,   27460,   27612,   27760,   27906,   28048,   28187,   28323,
+    28455,   28585,   28711,   28835,   28955,   29073,   29188,   29299,
+    29409,   29515,   29619,   29720,   29818,   29914,   30007,   30098,
+    30186,   30272,   30356,   30437,   30516,   30593,   30668,   30740,
+    30810,   30879,   30945,   31010,   31072,   31133,   31192,   31249,
+    31304,   31358,   31410,   31460,   31509,   31556,   31601,   31646,
+    31688,   31730,   31770,   31808,   31846,   31882,   31917,   31950,
+    31983,   32014,   32044,   32074,   32102,   32129,   32155,   32180,
+    32205,   32228,   32251,   32273,   32294,   32314,   32333,   32352,
+    32370,   32387,   32404,   32420,   32435,   32450,   32464,   32477,
+    32490,   32503,   32515,   32526,   32537,   32548,   32558,   32568,
+    32577,   32586,   32595,   32603,   32611,   32618,   32625,   32632,
+    32639,   32645,   32651,   32657,   32662,   32667,   32672,   32677,
+    32682,   32686,   32690,   32694,   32698,   32702,   32705,   32708,
+    32711,   32714,   32717,   32720,   32722,   32725,   32727,   32729,
+    32731,   32733,   32735,   32737,   32739,   32740,   32742,   32743,
+    32745,   32746,   32747,   32748,   32749,   32750,   32751,   32752,
+    32753,   32754,   32755,   32756,   32757,   32757,   32758,   32758,
+    32759,   32760,   32760,   32761,   32761,   32761,   32762,   32762,
+    32763,   32763,   32763,   32764,   32764,   32764,   32764,   32765,
+    32765,   32765,   32765,   32766,   32766,   32766,   32766,   32766 ]
+# Constants for the dungeon parameters
+MAX_HEIGHT = 66
+MAX_WIDTH = 198
+SCREEN_HEIGHT = 22
+SCREEN_WIDTH = 66
+DUN_ROOMS_MEAN = 32
 
 # Get the current unix time and store it in a clock variable.
 # Use the clock variable to parse a new random seed in
@@ -55,17 +102,45 @@ def rnd():
 def randomNumber(max):
 	return rnd() % max + 1
 
-# Note for Gaius -> is calling a function like this okay?
-# Seems scary to me, not sure why!
-seedsInitialize(0)
-print(rnd())
+# Generate a random number of normal distribution.
+def randomNumberNormalDistribution(mean, standard):
+	# Start by grabbing a random number from MAX_SHORT constant.
+	tmp = randomNumber(MAX_SHORT)
 
-# A few constant global variables we need to
-# declare.
-MAX_HEIGHT = 66
-MAX_WIDTH = 198
-SCREEN_HEIGHT = 22
-SCREEN_WIDTH = 66
+	# If tmp is equal to MAX_SHORT then re-randomize it
+	if tmp == MAX_SHORT:
+		offset = 4 * standard + randomNumber(standard)
+		if randomNumber(2) == 1:
+			offset = -offset
+		return mean + offset
+	
+	low = 0
+	iindex = NORMAL_TABLE_SIZE >> 1
+	high = NORMAL_TABLE_SIZE
+
+	# Loop through the values until we have a satisfactory value
+	while True:
+		if normal_table[iindex] == tmp or high == low + 1:
+			break
+		if normal_table[iindex] > tmp:
+			high = iindex
+			iindex = low + (iindex - low) >> 1
+		else:
+			low = iindex
+			iindex = iindex + ((high - iindex) >> 1)
+
+	# Check that we're not one below target
+	if normal_table[iindex] < tmp:
+		iindex = iindex + 1
+	
+	offset = ((standard * iindex) + (NORMAL_TABLE_SD >> 1)) / NORMAL_TABLE_SD
+
+	# Want some of the return values to be negative - such is how
+	# random normal distribution works.
+	if randomNumber(2) == 1:
+		offset = -offset
+	
+	return mean + offset
 
 # Coord_t
 class Coord_t:
@@ -111,8 +186,13 @@ def DungeonGenerate():
 	row_rooms = 2 * (dg.height / SCREEN_HEIGHT)
 	col_rooms = 2 * (dg.width / SCREEN_WIDTH)
 
-    # Create a new 20 x 20 room map.
-	room_map = [20], [20]
+	# Create a 2d array of rows and columns.
+	# room_map = [False], [False] * 20
+
+	# Set the array to return false.
+	random_room_count = randomNumberNormalDistribution(DUN_ROOMS_MEAN, 2)
+	print random_room_count
+
 
 
 def generateCave():
@@ -140,5 +220,8 @@ def generateCave():
     DungeonGenerate()
 
 
+def main():
+	seedsInitialize(0)
+	generateCave()
 
-generateCave()
+main()
