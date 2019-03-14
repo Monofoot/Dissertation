a89d0ccbefb027b1efa4156b6522be54d993da8a
Monofoot <epicwinningrob@gmail.com>
2019-03-14 00:49:12 +0000

    Spent too long on scripting a UI. I'm going to do a complete 180 tomorrow and scrap it completely. Will turn the Unity implementation into a fully built game as opposed to a developer tool. Major dissertation changes incoming.

--- src/map2unity.cs
+++ src/map2unity.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEditor;
 
 // The purpose of this script is to firstly create a GUI
 // for the user to input a text map file and secondly
@@ -13,6 +14,9 @@ using UnityEngine.UI;
 // another one. NO dependencies!
 
 public class map2unity : MonoBehaviour {
+
+	// This will be our text.
+	Text pathInputText;
 
 	// Maybe not have this in a function if it proves too difficult.
 	void SetupGui(){
@@ -54,27 +58,53 @@ public class map2unity : MonoBehaviour {
 		 userPromptTextTransform.localPosition = new Vector3(0, 0, 0);
 		 userPromptTextTransform.sizeDelta = new Vector2(800, 400);
 
-		 // Prompt the user for a file path.
+		// Prompt the user for a file path.
 		 GameObject path = new GameObject();
+		 GameObject pathText = new GameObject();
 		 path.transform.parent = canvasObject.transform;
 		 path.name = "User Input";
 		 RectTransform pathTransform = path.AddComponent<RectTransform>();
+		 pathTransform.sizeDelta = new Vector2(310, 60);
+		 pathTransform.anchoredPosition = new Vector2(0, 0); // I'm not sure how this works. Gonna be honest. But it does.
 		 CanvasRenderer pathRenderer = path.AddComponent<CanvasRenderer>();
 		 Image pathImage = path.AddComponent<Image>();
-		 pathImage.sprite = AssetDatabase.GetBuiltinExtraResource<Sprite>("UI/Skin/Background.psd"); // https://docs.unity3d.com/ScriptReference/Resources.Load.html Refer to this, work from here.
+		 pathImage.sprite = AssetDatabase.GetBuiltinExtraResource<Sprite>("UI/Skin/InputFieldBackground.psd"); // Good god this took me so long to find...
+		 pathImage.type = Image.Type.Sliced; 
 		 InputField pathInput = path.AddComponent<InputField>();
+		 pathInputText = pathText.AddComponent<Text>();
+		 pathText.transform.SetParent(pathInput.transform, false);
+		 pathInputText.name = "Text";
+		 pathInputText.font = Resources.GetBuiltinResource<Font>("Arial.ttf"); // This SHOULD be universal across Linux and Windows.
+		 pathInputText.supportRichText = false;
+		 pathInputText.color = Color.black;
+		 pathInput.textComponent = pathInputText;
+		 pathInput.ActivateInputField();
+		 pathInputText.text = pathInput.text;
 
-
+		// Button for submitting the file path.
+		 GameObject pButton = new GameObject();
+		 pButton.name = "Submit";
+		 RectTransform pathButtonTransform = pButton.AddComponent<RectTransform>();
+		 pathButtonTransform.sizeDelta = new Vector2(150, 50);
+		 Button pathButton = pButton.AddComponent<Button>();
+		 pathButton.transform.SetParent(canvas.transform, false);
+		 Image pathButtonImage = pButton.AddComponent<Image>();
+		 pathButtonImage.sprite = AssetDatabase.GetBuiltinExtraResource<Sprite>("UI/Skin/UISprite.psd");
+		 pathButton.onClick.AddListener(Clicky);
 
 		 
 	}
 
+	public void Clicky(){
+		Debug.Log("Clicked!");
+	}
+	
 	void Start () {
 		 // Prompt the user for a file path.
 		 SetupGui();
 	}
 	
 	void Update () {
-		
+		//Debug.Log(pathInputText.text);
 	}
 }
